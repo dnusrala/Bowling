1f8048f1168c6627fc8af82969cd68086bd5425d
Dilllon Nusrala <dnusrala@gmail.com>
2018-11-22 22:54:13 -0500

    Add project files.

---
+++ Bowling/BowlingGame.cs
@@ -0,0 +1,62 @@
+﻿using System;
+namespace Bowling {
+    public class BowlingGame {
+        private int _frame;
+        private int _ball;
+        private int _score;
+        private int _lastRollScore;
+        private int _mark;
+
+        public void Roll(int pins) {
+            if (pins > 10) {
+                throw new Exception("Pins cannot exceed 10");
+            }
+
+            if (pins < 0) {
+                throw new Exception("Pins cannot be negative");
+            }
+
+            if (_lastRollScore + pins > 10 && _ball == 1) {
+                throw new Exception("Single frame score cannot exceed 10");
+            }
+
+
+            if (_mark > 0) {
+                _score += pins;
+                _mark--;
+            }
+
+
+            _score = pins + _score;
+
+            //set fields for next roll
+
+            if (pins + _lastRollScore == 10 && _ball == 1) {
+                _mark = 1;
+            }
+
+            if (pins == 10 && _ball == 0) {
+                _mark = 2;
+                _frame++;
+                _ball = 0;
+                return;
+            }
+
+            if (_ball == 0) {
+                _ball++;
+            }
+            else {
+                _ball = 0;
+                _frame++;
+            }
+
+
+
+            _lastRollScore = pins;
+        }
+
+        public int GetScore() {
+            return _score;
+        }
+    }
+}
---
+++ BowlingTests/BowlingTests.cs
@@ -0,0 +1,82 @@
+﻿using NUnit.Framework;
+using Bowling;
+using System;
+
+namespace BowlingTests {
+    [TestFixture]
+    public class BowlingTests {
+
+        [Test]
+        public void ScoreStartsAtZero() {
+            var bowling = new BowlingGame();
+            var score = bowling.GetScore();
+            Assert.AreEqual(0, score);
+        }
+
+        [Test]
+        public void FirstRollPinCount() {
+            var bowling = new BowlingGame();
+            bowling.Roll(7);
+            var score = bowling.GetScore();
+            Assert.AreEqual(7, score);
+        }
+
+        [Test]
+        public void ScoreIsUnderTen() {
+            var bowling = new BowlingGame();
+            Assert.Throws<Exception>(() => bowling.Roll(11), "Pins cannot exceed 10");
+        }
+
+        [Test]
+        public void ScoreIsNegative() {
+            var bowling = new BowlingGame();
+            Assert.Throws<Exception>(() => bowling.Roll(-1), "Pins cannot be negative");
+        }
+
+        [Test]
+        public void SingleFrameScore() {
+            var bowling = new BowlingGame();
+            bowling.Roll(2);
+            bowling.Roll(1);
+            var score = bowling.GetScore();
+            Assert.AreEqual(3, score);
+        }
+
+        [Test]
+        public void SingleFrameScoreMaxTen() {
+            var bowling = new BowlingGame();
+            bowling.Roll(3);
+            Assert.Throws<Exception>(() => bowling.Roll(8), "Single frame score cannot exceed 10");
+
+        }
+        [Test]
+        public void ScoreAfterTwoFrames() {
+            var bowling = new BowlingGame();
+            bowling.Roll(7);
+            bowling.Roll(2);
+            bowling.Roll(5);
+            bowling.Roll(4);
+            var score = bowling.GetScore();
+            Assert.AreEqual(18, score);
+        }
+
+        [Test]
+        public void ScoreAfterSpare() {
+            var bowling = new BowlingGame();
+            bowling.Roll(1);
+            bowling.Roll(9);
+            bowling.Roll(5);
+            var score = bowling.GetScore();
+            Assert.AreEqual(20, score);
+        }
+        [Test]
+        public void ScoreAfterStrike() {
+            var bowling = new BowlingGame();
+            bowling.Roll(10);
+            bowling.Roll(4);
+            bowling.Roll(5);
+            var score = bowling.GetScore();
+            Assert.AreEqual(28, score);
+        }
+    }
+}
